0aac2f13679d3254b14ce7f7b9d43dabb854cad6
oliver <rodriguezoliverb@gmail.com>
2026-01-01 15:08:39 -0500

    Terminado el proyecto

--- script.js
+++ script.js
@@ -18,6 +18,8 @@
   });
 })(document); */
 
+/* **** Menu ***** */
+
 ((d) => {
   const $btnMenu = d.querySelector(".menu-btn"),
     $menu = d.querySelector(".menu");
@@ -34,5 +36,43 @@
     $btnMenu.firstElementChild.classList.remove("none");
     $btnMenu.lastElementChild.classList.add("none");
     $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* **** ConactForm ***** */
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/rodriguezoliverb@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        $loader.classList.add("none");
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}:${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
   });
 })(document);
